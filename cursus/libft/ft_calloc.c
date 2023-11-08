e7e20b9eec6a06810e40ae710813e0d51417654a
Florian Tilliet <ftilliet@paul-f3Br7s5.clusters.42paris.fr>
2023-11-08 18:20:31 +0100

    asd

--- cursus/libft/ft_calloc.c
+++ cursus/libft/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: ftilliet <ftilliet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 11:21:48 by ftilliet          #+#    #+#             */
-/*   Updated: 2023/11/08 17:45:44 by ftilliet         ###   ########.fr       */
+/*   Updated: 2023/11/08 18:20:21 by ftilliet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,12 @@
 void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void	*ptr;
-	size_t	i;
 
+	if ((nmemb * size < nmemb || size > nmemb * size) && size && nmemb)
+		return (NULL);
 	ptr = malloc(nmemb * size);
 	if (!ptr)
 		return (NULL);
-	i = 0;
-	ft_bzero(ptr, size);
+	ft_bzero(ptr, size * nmemb);
 	return (ptr);
 }
