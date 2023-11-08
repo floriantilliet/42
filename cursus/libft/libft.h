65e8b737a50948940d6305f4a68c30d56da43e12
Florian Tilliet <ftilliet@paul-f3Br7s5.clusters.42paris.fr>
2023-11-08 17:07:18 +0100

    asd

--- cursus/libft/libft.h
+++ cursus/libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: ftilliet <ftilliet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 14:36:12 by ftilliet          #+#    #+#             */
-/*   Updated: 2023/11/08 16:19:19 by ftilliet         ###   ########.fr       */
+/*   Updated: 2023/11/08 17:05:11 by ftilliet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,6 +51,8 @@ void			ft_lstdelone(t_list *lst, void (*del)(void *));
 void			ft_lstiter(t_list *lst, void (*f)(void *));
 
 t_list			*ft_lstlast(t_list *lst);
+
+t_list			*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 
 t_list			*ft_lstnew(void *content);
 
