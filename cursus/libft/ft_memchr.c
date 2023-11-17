49abfab5537bbd59a46c1581fd44e604803ecd7b
floriantilliet <florian.tilliet11@gmail.com>
2023-11-17 13:27:36 +0100

    qsd

--- cursus/libft/ft_memchr.c
+++ cursus/libft/ft_memchr.c
@@ -17,8 +17,6 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	size_t	i;
 	void	*ptr;
 
-	if (!s)
-		return (NULL);
 	i = 0;
 	while (i < n)
 	{
--- cursus/libft/ft_memset.c
+++ cursus/libft/ft_memset.c
@@ -16,8 +16,6 @@ void	*ft_memset(void *s, int c, size_t n)
 {
 	size_t	i;
 
-	if (!s)
-		return (NULL);
 	i = 0;
 	while (i < n)
 	{
