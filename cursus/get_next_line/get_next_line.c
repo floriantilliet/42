6740b2bccb73bcac85c9dca9d05a68d7763bf782
floriantilliet <florian.tilliet11@gmail.com>
2023-11-23 18:04:39 +0100

    qsd

--- cursus/get_next_line/get_next_line.c
+++ cursus/get_next_line/get_next_line.c
@@ -1,14 +1,37 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-size_t strlen(char* str)
+size_t ft_strlen(char* str)
 {   
     size_t  i;
     
     i = 0;
     while(str[i])
         i++;
+    return(i);
 }
+
+size_t	ft_strlcat(char *dest, char *src, size_t dstsize)
+{
+	size_t	c;
+	size_t	d;
+
+	if (!dest && dstsize == 0)
+		return (0);
+	if (dstsize <= ft_strlen(dest))
+		return (dstsize + ft_strlen(src));
+	c = ft_strlen(dest);
+	d = 0;
+	while (src[d] != '\0' && c + 1 < dstsize)
+	{
+		dest[c] = src[d];
+		c++;
+		d++;
+	}
+	dest[c] = '\0';
+	return (ft_strlen(dest) + ft_strlen(&src[d]));
+}
+
 
 int new_line(char* str)
 {
@@ -24,21 +47,50 @@ int new_line(char* str)
 char* clean(char* str)
 {
     int i;
+    int j;
+    char *stash;
     
     i = 0;
-    while
+    while(str[i] != '\n' || str[i])
+    {
+        if (!str[i])
+            return(str);
+        i++;
+    }
+    j = 0;
+    while(str[i+j])
+    {
+        j++;
+    }
+    stash = malloc(sizeof(char)* (j));
+    j=0;
+    while(str[i+j])
+    {
+        stash[j] = str[i+j];
+        j++;
+    }
+    return(stash);
 }
 
 char* get_new_line(char* str)
 {
     char* line;
+    int i;
     
+    i = 0;
+    while(str[i] != '\n')
+    {
+        i++;
+    }
+    line = malloc(sizeof(char) * i);
+    i = 0;
     while(str[i] != '\n')
     {
         line[i] = str[i];
         i++;
     }
     line[i] = '\0';
+    return(line);
 }
 
 char *get_next_line(int fd)
@@ -47,12 +99,31 @@ char *get_next_line(int fd)
     char* line;
     char* buffer;
     
+    buffer = malloc(sizeof(char) * 6);
     while(buffer != 0 || !new_line(stash))
     {
-        read(fd);
-        strlcat(stash,buffer,strlen(buffer));
+        read(fd, buffer, 5);
+        ft_strlcat(stash,buffer,ft_strlen(buffer));
     }
     line = get_new_line(stash);
     stash = clean(stash);
     return(line);
+}
+
+#include <stdio.h>
+#include <fcntl.h>
+
+int main(void)
+{
+    int fd;
+    char *line;
+
+    fd = open("test/simple", O_RDONLY);
+    while(1)
+    {
+        line = get_next_line(fd);
+        if (line == NULL)
+            break;
+        printf("%s", line);
+    }
 }
