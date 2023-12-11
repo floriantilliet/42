be190767d9fcceaad7df069de0917b0c0c5d2567
Florian Tilliet <ftilliet@made-f0Ar3s11.clusters.42paris.fr>
2023-12-11 12:20:59 +0100

    asd

--- cursus/fractol/main.c
+++ cursus/fractol/main.c
@@ -6,14 +6,28 @@
 
 #define MLX_ERROR 1
 
+int close(t_data *img)
+{
+    mlx_destroy_window(img->mlx_ptr, img->win_ptr);
+    exit(1);
+    return (0);
+}
+
 int	handle_keypress(int keysym, t_data *data)
 {
 	if (keysym == XK_Escape)
 	{
-		mlx_destroy_window(data->mlx_ptr, data->win_ptr);
+		close(data);
 		data->win_ptr = NULL;
 	}
+	printf("Keypress: %d\n", keysym);
 	return (0);
+}
+
+int	handle_keyrelease(int keysym, void *data)
+{
+    printf("Keyrelease: %d\n", keysym);
+    return (0);
 }
 
 int	render(t_data *data)
@@ -25,14 +39,14 @@ int	render(t_data *data)
 	if (data->win_ptr == NULL)
 		return (1);
 
-    color = ft_create_trgb(0,127,141,150);
+    color = ft_create_trgb(0,255,0,0);
     color = ft_get_opposite(color);
     color = ft_add_shade(0.5,color);
-    i = 250;
-    while(i<500)
+    i = 100;
+    while(i<200)
     {
-        j = 250;
-        while(j<500)
+        j = 100;
+        while(j<200)
         {
             ft_pixel_put(&data->img, i, j, color);
             j++;
@@ -44,14 +58,6 @@ int	render(t_data *data)
 
 	return (0);
 }
-
-int close(t_data *img)
-{
-    mlx_destroy_window(img->mlx_ptr, img->win_ptr);
-    exit(1);
-    return (0);
-}
-
 
 int	main(void)
 {
@@ -67,7 +73,6 @@ int	main(void)
 		return (MLX_ERROR);
 	}
 
-	/* Setup hooks */ 
 	data.img.mlx_img = mlx_new_image(data.mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT);
 	
 	data.img.addr = mlx_get_data_addr(data.img.mlx_img, &data.img.bpp,
@@ -75,11 +80,10 @@ int	main(void)
 
 	mlx_loop_hook(data.mlx_ptr, &render, &data);
 	mlx_hook(data.win_ptr, KeyPress, KeyPressMask, &handle_keypress, &data);
+	mlx_hook(data.win_ptr, KeyRelease, KeyReleaseMask, &handle_keyrelease, &data);
     mlx_hook(data.win_ptr, 17, 0L, close, &data);
 
 	mlx_loop(data.mlx_ptr);
-
-	/* we will exit the loop if there's no window left, and execute this code */
 	mlx_destroy_image(data.mlx_ptr, data.img.mlx_img);
 	mlx_destroy_display(data.mlx_ptr);
 	free(data.mlx_ptr);
