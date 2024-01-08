32741d95c79daeba527ca71a78743863e318f941
floriantilliet <florian.tilliet11@gmail.com>
2024-01-08 17:22:44 +0100

    asd

--- cursus/fdf/graphics.h
+++ cursus/fdf/graphics.h
@@ -6,7 +6,7 @@
 /*   By: florian <florian@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/31 17:30:48 by florian           #+#    #+#             */
-/*   Updated: 2024/01/08 17:20:46 by florian          ###   ########.fr       */
+/*   Updated: 2024/01/08 17:22:17 by florian          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,104 +32,108 @@
 
 typedef struct s_img
 {
-	void	*mlx_img;
-	char	*addr;
-	int		bpp;
-	int		line_len;
-	int		endian;
-}			t_img;
+	void		*mlx_img;
+	char		*addr;
+	int			bpp;
+	int			line_len;
+	int			endian;
+}				t_img;
 
 typedef struct s_point
 {
-	double	x;
-	double	y;
-	double	z;
-}			t_point;
+	double		x;
+	double		y;
+	double		z;
+}				t_point;
 
 typedef struct s_data
 {
-	void	*mlx_ptr;
-	void	*win_ptr;
-	t_img	img;
-	t_point	**map;
+	void		*mlx_ptr;
+	void		*win_ptr;
+	t_img		img;
+	t_point		**map;
 
-	double	angle_x;
-	double	angle_y;
-	double	angle_z;
+	double		angle_x;
+	double		angle_y;
+	double		angle_z;
 
-	double	offset_x;
-	double	offset_y;
+	double		offset_x;
+	double		offset_y;
 
-	double	zoom;
+	double		zoom;
 
-	int		mouse_button;
-	double	prev_x;
-	double	prev_y;
+	int			mouse_button;
+	double		prev_x;
+	double		prev_y;
 
-	double	ceiling;
-	double	floor;
-	double	height;
-	double	width;
-	double	ideal_zoom;
-}			t_data;
+	double		ceiling;
+	double		floor;
+	double		height;
+	double		width;
+	double		ideal_zoom;
+}				t_data;
 
 typedef struct s_matrix
 {
-	t_point	i;
-	t_point	j;
-	t_point	k;
-}			t_matrix;
+	t_point		i;
+	t_point		j;
+	t_point		k;
+}				t_matrix;
 
 typedef struct s_bresenham
 {
-	int		dx;
-	int		sx;
-	int		dy;
-	int		sy;
-	int		err;
-	t_point	start;
-	t_point	end;
-}			t_bresenham;
+	int			dx;
+	int			sx;
+	int			dy;
+	int			sy;
+	int			err;
+	t_point		start;
+	t_point		end;
+}				t_bresenham;
 
-typedef struct s_rot_matrices {
-	t_matrix rot_z;
-	t_matrix rot_x;
-	t_matrix rot_y;
-} t_rot_matrices;
+typedef struct s_rot_matrices
+{
+	t_matrix	rot_z;
+	t_matrix	rot_x;
+	t_matrix	rot_y;
+}				t_rot_matrices;
 
-int			ft_create_trgb(unsigned char t, unsigned char r, unsigned char g,
-				unsigned char b);
-void		ft_pixel_put(t_img *img, unsigned int x, unsigned int y, int color);
-void		ft_draw_line(t_data *data, t_point P0, t_point P1);
-t_data		ft_iso_projection(t_data *data);
-t_matrix	ft_get_rot_matrix(double deg, char axis);
-t_point		ft_multiply_vector_by_matrix(t_point *v, t_matrix m);
-double		interpolate_altitude(t_point start, t_point end, t_point current);
-char		**fd_to_tab(int fd);
-char		***tab_to_tabs(char **tab);
-t_point		**tabs_to_map(char ***tabs);
-void		ft_draw(t_data *data, t_data proj);
-void		get_limits(t_data *data);
-int			get_color(double z, double min_altitude, double max_altitude);
-int			key_hook(int keycode, t_data *data);
-int			arrow_move_hook(int keycode, t_data *data);
-int			key_rotation_hook(int keycode, t_data *data);
-int			key_reset_and_close_hook(int keycode, t_data *data);
-int			close_image(t_data *data);
-int			ft_mouse_down(int button, int x, int y, t_data *data);
-int			ft_mouse_up(int button, int x, int y, t_data *data);
-int			ft_mouse_move(int x, int y, t_data *data);
-void		ft_project_point(t_point *point, t_matrix rot_z, t_matrix rot_x,
-				t_matrix rot_y);
-void		ft_offset_and_zoom(t_point *point, t_data *data, int x, int y);
-double		interpolate_altitude(t_point start, t_point end, t_point current);
-void		update_err(t_bresenham *bres, t_point *P0);
-void		draw_line_loop(t_data *data, t_point *P0, t_point P1,
-				t_bresenham *bres);
-void	ft_allocate_proj_map(t_data *proj, int width, int height);
-t_point	ft_apply_single_transformation(t_point point, t_rot_matrices *rots,
-		t_data *data);
-void	ft_apply_transformations(t_data *proj, t_data *data,
-		t_rot_matrices *rots);					
+int				ft_create_trgb(unsigned char t, unsigned char r,
+					unsigned char g, unsigned char b);
+void			ft_pixel_put(t_img *img, unsigned int x, unsigned int y,
+					int color);
+void			ft_draw_line(t_data *data, t_point P0, t_point P1);
+t_data			ft_iso_projection(t_data *data);
+t_matrix		ft_get_rot_matrix(double deg, char axis);
+t_point			ft_multiply_vector_by_matrix(t_point *v, t_matrix m);
+double			interpolate_altitude(t_point start, t_point end,
+					t_point current);
+char			**fd_to_tab(int fd);
+char			***tab_to_tabs(char **tab);
+t_point			**tabs_to_map(char ***tabs);
+void			ft_draw(t_data *data, t_data proj);
+void			get_limits(t_data *data);
+int				get_color(double z, double min_altitude, double max_altitude);
+int				key_hook(int keycode, t_data *data);
+int				arrow_move_hook(int keycode, t_data *data);
+int				key_rotation_hook(int keycode, t_data *data);
+int				key_reset_and_close_hook(int keycode, t_data *data);
+int				close_image(t_data *data);
+int				ft_mouse_down(int button, int x, int y, t_data *data);
+int				ft_mouse_up(int button, int x, int y, t_data *data);
+int				ft_mouse_move(int x, int y, t_data *data);
+void			ft_project_point(t_point *point, t_matrix rot_z, t_matrix rot_x,
+					t_matrix rot_y);
+void			ft_offset_and_zoom(t_point *point, t_data *data, int x, int y);
+double			interpolate_altitude(t_point start, t_point end,
+					t_point current);
+void			update_err(t_bresenham *bres, t_point *P0);
+void			draw_line_loop(t_data *data, t_point *P0, t_point P1,
+					t_bresenham *bres);
+void			ft_allocate_proj_map(t_data *proj, int width, int height);
+t_point			ft_apply_single_transformation(t_point point,
+					t_rot_matrices *rots, t_data *data);
+void			ft_apply_transformations(t_data *proj, t_data *data,
+					t_rot_matrices *rots);
 
 #endif
---
+++ cursus/fdf/matrix.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   matrix.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: florian <florian@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/08 17:22:08 by florian           #+#    #+#             */
+/*   Updated: 2024/01/08 17:22:15 by florian          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "graphics.h"
+
+double	rad(double deg)
+{
+	return (deg * DEG_TO_RAD);
+}
+
+t_point	ft_multiply_vector_by_matrix(t_point *v, t_matrix m)
+{
+	t_point	res;
+
+	res.x = v->x * m.i.x + v->y * m.i.y + v->z * m.i.z;
+	res.y = v->x * m.j.x + v->y * m.j.y + v->z * m.j.z;
+	res.z = v->x * m.k.x + v->y * m.k.y + v->z * m.k.z;
+	return (res);
+}
+
+t_matrix	ft_get_rot_matrix(double deg, char axis)
+{
+	if (axis == 'x')
+		return ((t_matrix){(t_point){1., 0., 0.}, (t_point){0., cos(rad(deg)),
+			-sin(rad(deg))}, (t_point){0., sin(rad(deg)), cos(rad(deg))}});
+	if (axis == 'y')
+		return ((t_matrix){(t_point){cos(rad(deg)), 0., -sin(rad(deg))},
+			(t_point){0., 1., 0.}, (t_point){sin(deg), 0., cos(rad(deg))}});
+	if (axis == 'z')
+		return ((t_matrix){(t_point){cos(rad(deg)), -sin(rad(deg)), 0.},
+			(t_point){sin(rad(deg)), cos(rad(deg)), 0.}, (t_point){0., 0.,
+			1.}});
+	return ((t_matrix){
+		(t_point){1., 0., 0.},
+		(t_point){0., 1., 0.},
+		(t_point){0., 0., 1.},
+	});
+}
