cefb084828c7881cc34612562bd916bce1c70528
floriantilliet <florian.tilliet11@gmail.com>
2024-01-09 15:37:39 +0100

    asd

--- cursus/fdf/main.c
+++ cursus/fdf/main.c
@@ -6,7 +6,7 @@
 /*   By: florian <florian@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/18 15:20:50 by florian           #+#    #+#             */
-/*   Updated: 2024/01/09 15:21:29 by florian          ###   ########.fr       */
+/*   Updated: 2024/01/09 15:37:33 by florian          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,10 @@ int	main(int ac, char **av)
         return (0);
 
 	t_data	data;
-	ft_init_image(&data);
-	ft_get_map(&data, av[1]);
+	if (!ft_init_image(&data))
+		return (0);
+	if (!ft_get_map(&data, av[1]))
+		return (0);
 	ft_loop_hooks(&data);
+	return (0);
 }
