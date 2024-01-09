12bdfbf8ff92ab921d3c61d4a5e5471724ac679c
floriantilliet <florian.tilliet11@gmail.com>
2024-01-09 16:20:35 +0100

    asd

--- cursus/fdf/main.c
+++ cursus/fdf/main.c
@@ -6,7 +6,7 @@
 /*   By: florian <florian@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/18 15:20:50 by florian           #+#    #+#             */
-/*   Updated: 2024/01/09 15:51:12 by florian          ###   ########.fr       */
+/*   Updated: 2024/01/09 16:16:01 by florian          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,9 +32,9 @@ int	main(int ac, char **av)
 		return (0);
 	if (!ft_file_name_error(av[1]))
 		return (0);
-	if (!ft_init_image(&data))
-		return (0);
 	if (!ft_get_map(&data, av[1]))
+		return (0);
+	if (!ft_init_image(&data))
 		return (0);
 	ft_loop_hooks(&data);
 	return (0);
