67586a81ff021eeb4323bc1d6554b4d72ea9629b
floriantilliet <florian.tilliet11@gmail.com>
2024-01-22 17:33:43 +0100

    asd

--- cursus/push_swap/radix_sort.c
+++ cursus/push_swap/radix_sort.c
@@ -6,18 +6,33 @@
 /*   By: florian <florian@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/22 16:44:13 by florian           #+#    #+#             */
-/*   Updated: 2024/01/22 16:45:23 by florian          ###   ########.fr       */
+/*   Updated: 2024/01/22 17:33:38 by florian          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+void	sort_by_bit(t_stack_node **stackA, t_stack_node **stackB, int size,
+		int shift)
+{
+	int	i;
+
+	i = 0;
+	while (i < size)
+	{
+		if (((*stackA)->index >> shift) & 1)
+			ra(stackA);
+		else
+			pb(stackA, stackB);
+		i++;
+	}
+}
 
 void	radix_sort(t_stack_node **stackA, t_stack_node **stackB)
 {
 	int	size;
 	int	shift;
 	int	bit_max;
-	int	i;
 
 	bit_max = 0;
 	size = ft_double_lstsize(*stackA);
@@ -26,19 +41,11 @@ void	radix_sort(t_stack_node **stackA, t_stack_node **stackB)
 		size /= 2;
 		bit_max++;
 	}
-	size = ft_double_lstsize(*stackA);
 	shift = 0;
 	while (shift <= bit_max)
 	{
-		i = 0;
-		while (i < size)
-		{
-			if (((*stackA)->index >> shift) & 1)
-				ra(stackA);
-			else
-				pb(stackA, stackB);
-			i++;
-		}
+		size = ft_double_lstsize(*stackA);
+		sort_by_bit(stackA, stackB, size, shift);
 		shift++;
 		while (*stackB)
 			pa(stackA, stackB);
