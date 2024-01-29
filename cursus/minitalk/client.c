45407ecc88d4fcb6eace35242e14c1e2149bc93d
Florian Tilliet <ftilliet@made-f0Ar5s12.clusters.42paris.fr>
2024-01-29 16:04:02 +0100

    asd

--- cursus/minitalk/client.c
+++ cursus/minitalk/client.c
@@ -6,83 +6,77 @@
 /*   By: ftilliet <ftilliet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/23 13:21:50 by florian           #+#    #+#             */
-/*   Updated: 2024/01/29 15:17:36 by ftilliet         ###   ########.fr       */
+/*   Updated: 2024/01/29 16:03:21 by ftilliet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-int bit_received = 1;
+int		g_bit_received = 1;
 
-void sig_handler(int signum, siginfo_t* info, void* context)
+void	sig_handler(int signum, siginfo_t *info, void *context)
 {
-   (void)context;
-   (void)info;
-   (void)signum;
-   if (signum == SIGUSR2)
-      bit_received = 1;
-   if (signum == SIGUSR1)
-   {
-      write(1, "Message received\n", 17);
-      exit(0);
-   }
+	(void)context;
+	(void)info;
+	(void)signum;
+	if (signum == SIGUSR2)
+		g_bit_received = 1;
+	if (signum == SIGUSR1)
+	{
+		write(1, "Message received\n", 17);
+		exit(0);
+	}
 }
 
-void  str_to_bin(int pid, char *str)
+void	str_to_bin(int pid, char *str)
 {
 	int		i;
 	char	c;
 
-   if (bit_received)
-   {
-      bit_received = 0;
-      while (*str)
-      {
-         i = 7;
-         c = *str++;
-         while (i>=0)
-         {
-            if (c >> i & 1)
-               kill(pid, SIGUSR1);
-            else
-               kill(pid, SIGUSR2);
-            usleep(100);
-            i--;
-         }
-      }
-      if (*str == '\0')
-      {
-         i = 8;
-         while (i--)
-         {
-            kill(pid, SIGUSR2);
-            usleep(100);
-         }
-      }
-   }
-   // else
-   // {
-   //    i = 0;
-   //    while(i++ < 50)
-   //       usleep(100);
-   // }
+	if (g_bit_received)
+	{
+		g_bit_received = 0;
+		while (*str)
+		{
+			i = 7;
+			c = *str++;
+			while (i >= 0)
+			{
+				if (c >> i & 1)
+					kill(pid, SIGUSR1);
+				else
+					kill(pid, SIGUSR2);
+				usleep(100);
+				i--;
+			}
+		}
+		if (*str == '\0')
+		{
+			i = 8;
+			while (i--)
+			{
+				kill(pid, SIGUSR2);
+				usleep(100);
+			}
+		}
+	}
 }
 
-int main(int ac, char** av)
+int	main(int ac, char **av)
 {
-   struct sigaction sa;
-   int pid;
-   ac++;
+	struct sigaction	sa;
+	int					pid;
 
-   sigemptyset(&sa.sa_mask);
-   sa.sa_flags = SA_RESTART | SA_SIGINFO;
-   sa.sa_sigaction = sig_handler;
-   pid = atoi(av[1]);
-   sigaction(SIGUSR1, &sa, NULL);
-   sigaction(SIGUSR2, &sa, NULL);
-   str_to_bin(pid, av[2]);
-   str_to_bin(pid, "\0");
-   while (1)
-      sleep(1);
-   return (0);
+	(void)ac++;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = SA_RESTART | SA_SIGINFO;
+	sa.sa_sigaction = sig_handler;
+	pid = atoi(av[1]);
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
+	str_to_bin(pid, av[2]);
+	str_to_bin(pid, "\0");
+	while (1)
+		sleep(1);
+	return (0);
 }
--- cursus/minitalk/server.c
+++ cursus/minitalk/server.c
@@ -6,65 +6,65 @@
 /*   By: ftilliet <ftilliet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/23 17:04:43 by florian           #+#    #+#             */
-/*   Updated: 2024/01/29 15:16:05 by ftilliet         ###   ########.fr       */
+/*   Updated: 2024/01/29 16:01:20 by ftilliet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 #include <stdio.h>
 
-void bin_to_char(int signum, char *c)
+void	bin_to_char(int signum, char *c)
 {
-   if (signum == SIGUSR1)
-      *c = (*c << 1) | 1;
-   else if (signum == SIGUSR2)
-      *c = (*c << 1) | 0;
+	if (signum == SIGUSR1)
+		*c = (*c << 1) | 1;
+	else if (signum == SIGUSR2)
+		*c = (*c << 1) | 0;
 }
 
-void sig_handler(int signum, siginfo_t* info, void* context)
+void	sig_handler(int signum, siginfo_t *info, void *context)
 {
-   static char c;
-   static char *str;
-   static int i;
+	static char	c;
+	static char	*str;
+	static int	i;
 
-   (void)context;
-   (void)info;
-   bin_to_char(signum, &c);
-   if (i == 7)
-   {
-      i = 0;
-      if (c == '\0')
-      {
-         write(1, str, ft_strlen(str));
-         write(1, "\n", 1);
-         free(str);
-         str = NULL;
-         kill(info->si_pid, SIGUSR1);
-         return ;
-      }
-      if (!str)
+	(void)context;
+	(void)info;
+	bin_to_char(signum, &c);
+	if (i == 7)
+	{
+		i = 0;
+		if (c == '\0')
+		{
+			write(1, str, ft_strlen(str));
+			write(1, "\n", 1);
+			free(str);
+			str = NULL;
+			kill(info->si_pid, SIGUSR1);
+			return ;
+		}
+		if (!str)
 			str = ft_strdup(&c);
-      else
-         str = ft_strjoin(str, &c);
-   }
-   else
-      i++;
-   kill(info->si_pid, SIGUSR2);
+		else
+			str = ft_strjoin(str, &c);
+	}
+	else
+		i++;
+	kill(info->si_pid, SIGUSR2);
 }
 
-int main(void)
+int	main(void)
 {
-   struct sigaction sa;
-  
-   sigemptyset(&sa.sa_mask);
-   sa.sa_flags = SA_RESTART | SA_SIGINFO;
-   sa.sa_sigaction = sig_handler;
-   write(1, "PID: ", 5);
-   ft_putnbr_fd(getpid(), 1);
-   write(1, "\n", 1);
-   sigaction(SIGUSR1, &sa, NULL);
-   sigaction(SIGUSR2, &sa, NULL);
-   while (1)
-     pause();
-   return (0);
+	struct sigaction	sa;
+
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = SA_RESTART | SA_SIGINFO;
+	sa.sa_sigaction = sig_handler;
+	write(1, "PID: ", 5);
+	ft_putnbr_fd(getpid(), 1);
+	write(1, "\n", 1);
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
+	while (1)
+		pause();
+	return (0);
 }
