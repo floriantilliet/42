5f4f648026a2f0eed6499800ea29491a7b5f842c
Florian Tilliet <ftilliet@bess-f1r2s10.clusters.42paris.fr>
2024-03-07 12:53:03 +0100

    asd

--- cursus/philosophers/main.c
+++ cursus/philosophers/main.c
@@ -6,7 +6,7 @@
 /*   By: ftilliet <ftilliet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/12 11:09:06 by ftilliet          #+#    #+#             */
-/*   Updated: 2024/03/07 10:36:11 by ftilliet         ###   ########.fr       */
+/*   Updated: 2024/03/07 12:52:56 by ftilliet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,31 +45,72 @@ void init_mutexes(pthread_mutex_t *mutexes, int nb)
 	}
 }
 
-void init_philos(pthread_t *philos, int nb)
+void init_philos(t_philo *philos, t_data data, int nb)
 {
-	
+	philos = malloc(sizeof(t_philo) * nb);
+	int	i;
+
+	i = 0;
+	while (i < nb)
+	{
+		philos[i].meals_eaten = 0;
+		philos[i].is_dead = 0;
+		philos[i].r_fork = &data.mutexes[i];
+		philos[i].l_fork = &data.mutexes[(i+1) % nb];
+		i++;
+	}
+}
+
+void thread_create(t_philo *philos, t_data data, int nb)
+{
+	int	i;
+
+	i = 0;
+	while (i < nb)
+	{
+		pthread_create(&philos[i].thread, NULL, routine, &data);
+		i++;
+	}
+	i = 0;
+	while (i < nb)
+	{
+		pthread_join(philos[i].thread, NULL);
+		i++;
+	}
 }
 
 void *routine(void *arg)
 {   
-    struct s_data *data = (struct s_data *)arg;
+    t_philo *philo = (t_philo *)philo;
 
-	while(!dead)
+	while(!(philo->data->is_dead))
 	{
-		pthread_mutex_lock(data->);
-		pthread_mutex_lock(data->);
+		pthread_mutex_lock(philo->r_fork);
+		pthread_mutex_lock(philo->l_fork);
 		eat();
-		pthread_mutex_unlock(data->);
-		pthread_mutex_unlock(data->);
+		pthread_mutex_unlock(philo->l_fork);
+		pthread_mutex_unlock(philo->r_fork);
 		sleep();
 		think();
 	}
 }
 
+void init_data(t_data data, char **av, int ac)
+{
+	if (ac == 5)
+		data.nb_of_meals = -1;
+	else
+		data.nb_of_meals = ft_atoi(av[6]);
+	data.time_to_eat = ft_atoi(av[3]);
+	data.time_to_sleep = ft_atoi(av[4]);
+	data.time_to_die = ft_atoi(av[2]);
+	data.nb_of_philos = ft_atoi(av[1]);
+}
+
 int	main(int ac, char **av)
 {	
-	pthread_mutex_t	*mutexes;
-	pthread_t		*philos;
+	t_data data;
+	t_philo *philos;
 	
     if (ac != 5 && ac != 6)
         return (ft_putendl_fd("Error", 2), 0);
@@ -77,9 +118,10 @@ int	main(int ac, char **av)
     {
         return (0);
     }
-	mutexes = NULL;
-	init_mutexes(mutexes, ft_atoi(av[1]));
-	philos = malloc(sizeof(pthread_t) * ft_atoi(av[1]));
+	init_data(data, av, ac);
+	data.mutexes = NULL;
+	init_mutexes(data.mutexes, ft_atoi(av[1]));
+	init_philos(philos, data, ft_atoi(av[1]));
 	
 	return (0);
 }
--- cursus/philosophers/philosophers.h
+++ cursus/philosophers/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: ftilliet <ftilliet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/12 17:24:13 by ftilliet          #+#    #+#             */
-/*   Updated: 2024/03/07 10:29:17 by ftilliet         ###   ########.fr       */
+/*   Updated: 2024/03/07 12:51:52 by ftilliet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,17 +23,22 @@ typedef struct s_philo
 	int				meals_eaten;
 	pthread_mutex_t	*r_fork;
 	pthread_mutex_t	*l_fork;
+	pthread_t		thread;
+	t_data			*data;
 }					t_philo;
 
 typedef struct s_data
 {
-    int time_to_eat;
-    int time_to_sleep;
-    int time_to_think;
-    int time_to_die;
-    int t0;
-
-    t_philo *philos;
+	int				time_to_eat;
+	int				time_to_sleep;
+	int				time_to_think;
+	int				time_to_die;
+	int				t0;
+	int				nb_of_philos;
+	int				nb_of_meals;
+	int				is_dead;
+	t_philo			*philos;
+	pthread_mutex_t	*mutexes;
 }					t_data;
 
 #endif
