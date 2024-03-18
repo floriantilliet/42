25f9c1e6d889f9c5fc7d091ddfa7148244c62c39
Florian Tilliet <ftilliet@bess-f1r3s11.clusters.42paris.fr>
2024-03-18 19:28:32 +0100

    asd

--- cursus/philosophers/routine.c
+++ cursus/philosophers/routine.c
@@ -6,7 +6,7 @@
 /*   By: ftilliet <ftilliet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/18 17:42:24 by ftilliet          #+#    #+#             */
-/*   Updated: 2024/03/18 19:11:41 by ftilliet         ###   ########.fr       */
+/*   Updated: 2024/03/18 19:27:21 by ftilliet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,15 @@
 
 void	eat(t_philo *philo)
 {
+    pthread_mutex_lock(&philo->data->time);
+	philo->time_last_meal = get_current_time();
+	pthread_mutex_unlock(&philo->data->time);
 	print_state("is eating", philo);
 	ft_usleep(philo->data->time_to_eat);
 	if (philo->meals_to_eat > 0)
 	{
 		philo->meals_to_eat--;
 	}
-	pthread_mutex_lock(&philo->data->time);
-	philo->time_last_meal = get_current_time();
-	pthread_mutex_unlock(&philo->data->time);
 }
 
 void	dream(t_philo *philo)
