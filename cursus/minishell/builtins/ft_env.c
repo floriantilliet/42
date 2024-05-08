d9dddb74b17ec4e3c1248927f5510991cf74e02b
floriantilliet <ninjadargent@gmail.com>
2024-05-08 12:05:42 +0200

    asd

--- cursus/minishell/builtins/ft_env.c
+++ cursus/minishell/builtins/ft_env.c
@@ -6,12 +6,13 @@
 /*   By: florian <florian@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/08 11:40:47 by florian           #+#    #+#             */
-/*   Updated: 2024/05/08 11:42:53 by florian          ###   ########.fr       */
+/*   Updated: 2024/05/08 11:58:09 by florian          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
+// doit print dans l'ordre ascii!
 void	printenv(t_env **env)
 {
 	t_env *current;
