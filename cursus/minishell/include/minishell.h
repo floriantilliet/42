99f2e7d66a6910cb3e41b679a558010dc254f632
floriantilliet <ninjadargent@gmail.com>
2024-06-04 14:53:37 +0200

    asd

--- cursus/minishell/include/minishell.h
+++ cursus/minishell/include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: florian <florian@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/27 05:43:39 by ftilliet          #+#    #+#             */
-/*   Updated: 2024/06/04 13:33:38 by florian          ###   ########.fr       */
+/*   Updated: 2024/06/04 14:53:30 by florian          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,35 +53,75 @@ typedef struct s_flags
 	int				pipe_flag;
 }					t_flags;
 
-// env.c
+// builtins
 
-t_env				*create_env_node(char *env_str);
-void				append_env_node(t_env **env, t_env *new_node);
-t_env				**store_env(char **envp);
+int					ft_pwd(void);
 int					printenv(t_env **env);
-char				*get_env_value(char *key, t_env **env);
 
-// expander.c
+// lexing
 
-char				*expander(char *line, t_env **env);
-
-// lexer.c
-
-int					is_space(char c);
-char				**line_to_strings(char *line);
-void				print_tokens(char **tokens);
-int					check_quote_problems(char *line);
+int					handle_token_quotes(char *line, int *i);
+int					handle_special_chars(char *line, int *i);
+int					handle_normal_chars(char *line, int *i);
+int					handle_spaces(char *line, int *i);
 int					count_tokens(char *line);
-t_token				**strings_to_tokens(char **tokens);
-void				print_token_list(t_token **token_list);
-int					get_token_type(char *token);
+
+char				*trimmer(char *str);
 void				expand_token(t_token *current, t_env **env);
 void				trim_token(t_token *current);
 void				expand_token_list(t_token **token_list, t_env **env);
-int					check_pipe_problems(char *line);
+
+void				handle_exp_quotes(char *line, char *current_quote,
+						int *expand, int *i);
+void				append_char(char **res, char *line, int *i);
+void				handle_expansion(char **res, char *line, int *i,
+						t_env **env);
+char				*expander(char *line, t_env **env);
+
+int					is_space(char c);
+void				print_strings(char **tokens);
+void				print_token_list(t_token **token_list);
+
+void				handle_string_quotes(char *line, char **tokens, int *i,
+						int *j);
+void				handle_redirection(char *line, char **tokens, int *i,
+						int *j);
+void				handle_space(char *line, char **tokens, int *i, int *j);
+void				handle_general(char *line, char **tokens, int *i, int *j);
+char				**line_to_strings(char *line);
+
+void				update_arg_cmd_flags(t_token *new_node, t_flags *flags);
+void				update_token_flags(t_token *new_node, t_flags *flags);
+
+void				process_arg_cmd(t_token **current, t_token **new_list,
+						t_token **last_new_node, t_flags *flags);
+void				copy_non_arg_cmd(t_token **current, t_token **new_list,
+						t_token **last_new_node, t_flags *flags);
+void				process_current_token(t_token **current, t_token **new_list,
+						t_token **last_new_node, t_flags *flags);
+t_token				**merge_tokens(t_token **token_list);
+
+void				free_new_list(t_token *new_list);
+char				*merge_values(char *merged_value, char *next_value);
+t_token				**create_merged_list(t_token *new_list);
+void				initialize_merge_vars(t_token **current,
+						t_token **token_list, t_flags *flags);
+void				add_new_node(t_token **new_list, t_token **last_new_node,
+						char *value, int type);
+
+int					check_quote_problems(char *line);
 int					redirection_flag_updater(char *line, int *i, int len);
 int					check_redirection_problems(char *line);
+int					check_pipe_problems(char *line);
 int					check_problems(char *line);
+
+t_token				*create_token_node(char *value);
+void				add_token_to_list(t_token **token_list, t_token *new_node);
+void				update_flags(t_token *new_node, int *cmd_flag,
+						int *redirection_flag);
+t_token				**strings_to_tokens(char **tokens);
+
+int					check_token_chars(char *token, int len);
 int					get_token_type(char *token);
 
 // cleaning_utils.c
@@ -90,27 +130,16 @@ void				free_char_tab(char **tab);
 void				free_env(t_env **env);
 void				free_token_list(t_token **token_list);
 
+// env.c
+
+t_env				*create_env_node(char *env_str);
+void				append_env_node(t_env **env, t_env *new_node);
+t_env				**store_env(char **envp);
+char				*get_env_value(char *key, t_env **env);
+
 // signals.c
 
+void				handle_sigint(int code);
 void				signals(void);
-void				sigint_handler(int code);
-
-// builtins
-
-int					ft_pwd(void);
-
-void				print_strings(char **tokens);
-t_token				**merge_tokens(t_token **token_list);
-void				free_new_list(t_token *new_list);
-char				*merge_values(char *merged_value, char *next_value);
-void				add_new_node(t_token **new_list, t_token **last_new_node,
-						char *value, int type);
-void				update_arg_cmd_flags(t_token *new_node, t_flags *flags);
-void				update_token_flags(t_token *new_node, t_flags *flags);
-void				update_arg_cmd_flags(t_token *new_node, t_flags *flags);
-void				update_token_flags(t_token *new_node, t_flags *flags);
-void				initialize_merge_vars(t_token **current,
-						t_token **token_list, t_flags *flags);
-t_token				**create_merged_list(t_token *new_list);
 
 #endif
