eeda107fa315813e179e6f406c7a4b7fb1a58a4f
floriantilliet <florian.tilliet11@gmail.com>
2024-10-13 14:37:48 +0200

    minirt

--- cursus/miniRT/main.c
+++ cursus/miniRT/main.c
@@ -6,7 +6,7 @@
 /*   By: florian <florian@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 19:31:59 by florian           #+#    #+#             */
-/*   Updated: 2024/10/13 13:55:22 by florian          ###   ########.fr       */
+/*   Updated: 2024/10/13 14:13:44 by florian          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,38 +61,38 @@ int	main(int ac, char **av)
 
     // print_intersections(new_object->intersections);
     
-    // int color = ft_create_trgb(0, 255, 0, 0);
+    int color = ft_create_trgb(0, 255, 0, 0);
     ft_init_image(&data);
 
-    new_object->transformation = mat_product(scaling_mat(1,0.5,1), rotation_z_mat(PI/5));
-    t_tuple normal = sphere_normal(new_object, create_point(0,sqrt(2)/2, -sqrt(2)/2));
-    printf("x: %f, y: %f, z: %f\n", normal.x, normal.y, normal.z);
+    // new_object->transformation = mat_product(scaling_mat(1,0.5,1), rotation_z_mat(PI/5));
+    // t_tuple normal = sphere_normal(new_object, create_point(0,sqrt(2)/2, -sqrt(2)/2));
+    // printf("x: %f, y: %f, z: %f\n", normal.x, normal.y, normal.z);
     
-    // int y = 0;
-    // int x = 0;
-    // float world_y;
-    // float world_x;
-    // float wall_size = 7;
-    // float pixel_size =wall_size / WINDOW_WIDTH;
-    // while (y < WINDOW_HEIGHT - 1)
-    // {
-    //     world_y = (wall_size / 2) - y * pixel_size;
-    //     x = 0;
-    //     while (x < WINDOW_WIDTH - 1)
-    //     {
-    //         world_x = - (wall_size/2) + x * pixel_size;
-    //         t_tuple position = create_point(world_x, world_y, 10);
-    //         t_tuple ray_origin = create_point(0, 0, -5);
-    //         t_ray ray = create_ray(ray_origin, normalize_vector(substract_floats(position, ray_origin)));
-    //         if (sphere_intersections(ray, new_object))
-    //             ft_pixel_put(&data.img, x, y, color);
-    //         // if (hit(new_object->intersections))
-    //             // ft_pixel_put(&data.img, x, y, color);
-    //         x++;
-    //     }
-    //     y++;
-    // }
-    // printf("Done\n");
+    int y = 0;
+    int x = 0;
+    float world_y;
+    float world_x;
+    float wall_size = 7;
+    float pixel_size =wall_size / WINDOW_WIDTH;
+    while (y < WINDOW_HEIGHT - 1)
+    {
+        world_y = (wall_size / 2) - y * pixel_size;
+        x = 0;
+        while (x < WINDOW_WIDTH - 1)
+        {
+            world_x = - (wall_size/2) + x * pixel_size;
+            t_tuple position = create_point(world_x, world_y, 10);
+            t_tuple ray_origin = create_point(0, 0, -5);
+            t_ray ray = create_ray(ray_origin, normalize_vector(substract_floats(position, ray_origin)));
+            if (sphere_intersections(ray, new_object))
+                ft_pixel_put(&data.img, x, y, color);
+            // if (hit(new_object->intersections))
+                // ft_pixel_put(&data.img, x, y, color);
+            x++;
+        }
+        y++;
+    }
+    printf("Done\n");
     // ft_pixel_put(&data.img, 1, 1, color);
     // ft_pixel_put(&data.img, 2, 2, color);
     // ft_pixel_put(&data.img, 3, 3, color);
